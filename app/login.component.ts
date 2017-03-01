d7065cd2b363622931a0ec06ab75df98598ac53a
minmiguelm <cmiguelmg@gmail.com>
2017-03-01 15:35:23 -0500

    creacion del login

---
+++ app/login.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+    moduleId: module.id,
+    selector: 'login',
+    templateUrl: 'html/login.component.html',
+    styleUrls:['style/login.component.css']
+})
+export class LoginComponent {
+
+}
+
